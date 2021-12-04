de67eb8616ce320794a4548dbb42b94eb44d6d68
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-12-04 17:20:43 +0400

    added login button

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,28 +1,34 @@
 <nav class="p-2">
-    <span>Earthify</span>
+    <span class="title">Earthify</span>
 
     <a href="/">Home</a>
     <a href="/starred">Starred Listings</a>
     <a href="/chats">Chats</a>
     <a href="/manage">Manage Listings</a>
+
+    <a href="/#" class="auth">Login</a>
 </nav>
 <br />
 <br />
 
 <style>
+    .title {
+        font-weight: bold;
+        font-size: x-large;
+        padding: 0 1.7% 0 1.7%;
+        color: white;
+    }
+
+    .auth {
+        float: right;
+    }
+
     nav {
         background-color: #30d158; /* TODO: Change this later */
         position: fixed;
         width: 100%;
         top: 0;
         left: 0;
-    }
-
-    span {
-        font-weight: bold;
-        font-size: x-large;
-        padding: 0 1.7% 0 1.7%;
-        color: white;
     }
 
     a {
